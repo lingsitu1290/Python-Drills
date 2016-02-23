14c9f977aeae0be1e4fa75504ef5c7aa236cebbb
Ling Situ <lingsitu1290@gmail.com>
2016-02-22 21:27:15 -0800

    Initial Commit

---
+++ shutil1 (1).py
@@ -0,0 +1,24 @@
+import shutil
+import os
+import time
+import datetime
+
+
+for name in os.listdir('C:\Users\Student\Desktop\A'):
+    src = 'C:\Users\Student\Desktop\A\{}'.format(name)
+    dst = 'C:\Users\Student\Desktop\B\{}'.format(name)
+
+    #now = time.time()
+    #print now
+    time_diff = time.time() - (os.path.getmtime(src))
+
+    _24hoursago = time.time() - (24 *60 *60)
+    last_24_hrs = time.time() - _24hoursago 
+
+
+    if time_diff < last_24_hrs:
+        print 'File moved: {} '.format(src)
+        print "last modified: {}".format(time.ctime(os.path.getmtime(src)))
+        shutil.move(src, dst)
+        
+    
